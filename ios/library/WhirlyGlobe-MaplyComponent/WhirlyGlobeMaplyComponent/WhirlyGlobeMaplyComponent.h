fe39b7b3ce4ddd2d4952fe25be1c17b801336b25
Stephen Lewis <steve@eaglecapsoftware.com>
2020-08-03 13:32:43 -0600

    Update project settings for building as a subproject.

--- ios/library/WhirlyGlobe-MaplyComponent/WhirlyGlobeMaplyComponent/WhirlyGlobeMaplyComponent.h
+++ ios/library/WhirlyGlobe-MaplyComponent/WhirlyGlobeMaplyComponent/WhirlyGlobeMaplyComponent.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char WhirlyGlobeMaplyComponentVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WhirlyGlobeMaplyComponent/PublicHeader.h>
 
-
+#import <WhirlyGlobeMaplyComponent/WhirlyGlobeComponent.h>
